738766ad93813efcc7c2c91dd42d411c642eb60c
midfinup <steammfu@yandex.ru>
2023-02-27 20:51:17 +0700

    added new buttons without functional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
 
-from telegram import __version__ as TG_VER
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes
@@ -17,10 +16,15 @@ TOKEN = "5818761953:AAEVwWY4agQmQyVHCUYra8S5MN4crl7chGQ"
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [
         [
-            InlineKeyboardButton("Афиша", callback_data="1"),
-            InlineKeyboardButton("О нас", callback_data="2"),
+            InlineKeyboardButton("Афиша", callback_data="poster"),
+            InlineKeyboardButton("О нас", callback_data="about_us"),
         ],
-        [InlineKeyboardButton("Купить билеты", callback_data="3")],
+        [
+            InlineKeyboardButton("Купить билеты", callback_data="buy_ticket")
+        ],
+        [
+            InlineKeyboardButton("ДА?", callback_data="other_funcs")
+        ],
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
@@ -28,11 +32,90 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Привет! \n blabla\n :)", reply_markup=reply_markup)
 
 
+async def main_page(query) -> None:
+    keyboard = [
+        [
+            InlineKeyboardButton("Афиша", callback_data="poster"),
+            InlineKeyboardButton("О нас", callback_data="about_us"),
+        ],
+        [
+            InlineKeyboardButton("Купить Билет", callback_data="buy_ticket")
+        ],
+        [
+            InlineKeyboardButton("ДА?", callback_data="other_funcs")
+        ],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await query.edit_message_text("Привет! \n blabla\n :)", reply_markup=reply_markup)
+
+
+async def about_us(query) -> None:
+    keyboard = [
+        [
+            InlineKeyboardButton("На Главную", callback_data="main")
+        ],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await query.edit_message_text("***Информация про КБрд***", reply_markup=reply_markup)
+
+
+async def poster(query) -> None:
+    keyboard = [
+        [
+            InlineKeyboardButton("На Главную", callback_data="main")
+        ],
+        [
+            InlineKeyboardButton("Купить Билет", callback_data="buy_ticket")
+        ]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await query.edit_message_text("***Афиша КБрд***", reply_markup=reply_markup)
+
+
+async def buy_ticket(query) -> None:
+    keyboard = [
+        [
+            InlineKeyboardButton("На Главную", callback_data="main")
+        ],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await query.edit_message_text("***Операция покупки билета***", reply_markup=reply_markup)
+
+
+async def other_funcs(query) -> None:
+    keyboard = [
+        [
+            InlineKeyboardButton("На Главную", callback_data="main")
+        ],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await query.edit_message_text("ПИЗДА", reply_markup=reply_markup)
+
+
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     await query.answer()
-
-    await query.edit_message_text(text=f"Selected option: {query.data}")
+    if query.data == "about_us":
+        await about_us(query)
+    if query.data == "main":
+        await main_page(query)
+    if query.data == "poster":
+        await poster(query)
+    if query.data == "buy_ticket":
+        await buy_ticket(query)
+    if query.data == "other_funcs":
+        await other_funcs(query)
+    # await query.edit_message_text(text=f"Selected option: {query.data}")
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
