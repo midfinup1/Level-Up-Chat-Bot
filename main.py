3100a99560f4f889bb0c920ec5c2c0b92fdb0d1d
midfinup <steammfu@yandex.ru>
2023-03-05 18:12:23 +0700

    organization improved; relocated some buttons

--- main.py
+++ main.py
@@ -12,94 +12,126 @@ logger = logging.getLogger(__name__)
 
 TOKEN = "5818761953:AAEVwWY4agQmQyVHCUYra8S5MN4crl7chGQ"
 
+buttons = {
+    "poster": InlineKeyboardButton("Афиша", callback_data="poster"),
+    "support": InlineKeyboardButton("Поддержка", callback_data="support"),
+    "about_us": InlineKeyboardButton("О нас", callback_data="about_us"),
+    "other_funcs": InlineKeyboardButton("Другое", callback_data="other_funcs"),
+    "main": InlineKeyboardButton("🏠На Главную", callback_data="main"),
+    "buy_ticket": InlineKeyboardButton("Купить Билет", callback_data="buy_ticket"),
+    "about_event": InlineKeyboardButton("О мероприятии", callback_data="about_event"),
+    "back_to_poster": InlineKeyboardButton("↩Назад", callback_data="back_to_poster")
+}
+
+texts = {
+    "main": "Привет! \n blabla\t\tblabla\t\tblabla\t\tblabla\t\tblablablablablabla.\n :)",
+    "support": "***Поддержим еблана👍***",
+    "about_us": "***Информация про КБрд***",
+    "poster": "***Афиша КБрд***",
+    "buy_ticket": "***Операция покупки билета***",
+    "about_event": "***О мероприятии***",
+    "other_funcs": "ТУТ ПУСТО ЁПТА"
+}
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [
         [
-            InlineKeyboardButton("Афиша", callback_data="poster"),
-            InlineKeyboardButton("О нас", callback_data="about_us"),
+            buttons["poster"]
         ],
         [
-            InlineKeyboardButton("Купить билеты", callback_data="buy_ticket")
+            buttons["support"],
+            buttons["about_us"]
         ],
         [
-            InlineKeyboardButton("ДА?", callback_data="other_funcs")
-        ],
+            buttons["back_to_poster"]
+        ]
     ]
-
     reply_markup = InlineKeyboardMarkup(keyboard)
-
-    await update.message.reply_text("Привет! \n blabla\t\tblabla\t\tblabla\t\tblabla\t\tblablablablablabla.\n :)", reply_markup=reply_markup)
+    await update.message.reply_text(texts["main"], reply_markup=reply_markup)
 
 
 async def main_page(query) -> None:
     keyboard = [
         [
-            InlineKeyboardButton("Афиша", callback_data="poster"),
-            InlineKeyboardButton("О нас", callback_data="about_us"),
+            buttons["poster"]
         ],
         [
-            InlineKeyboardButton("Купить Билет", callback_data="buy_ticket")
+            buttons["support"],
+            buttons["about_us"]
         ],
         [
-            InlineKeyboardButton("ДА?", callback_data="other_funcs")
-        ],
+            buttons["other_funcs"]
+        ]
     ]
-
     reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text(texts["main"], reply_markup=reply_markup)
 
-    await query.edit_message_text("Привет! \n blabla\t\tblabla\t\tblabla\t\tblabla\t\tblablablabla\n :)", reply_markup=reply_markup)
+
+async def support(query) -> None:
+    keyboard = [
+        [
+            buttons["main"]
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text(texts["support"], reply_markup=reply_markup)
 
 
 async def about_us(query) -> None:
     keyboard = [
         [
-            InlineKeyboardButton("На Главную", callback_data="main")
-        ],
+            buttons["main"]
+        ]
     ]
-
     reply_markup = InlineKeyboardMarkup(keyboard)
-
-    await query.edit_message_text("***Информация про КБрд***", reply_markup=reply_markup)
+    await query.edit_message_text(texts["about_us"], reply_markup=reply_markup)
 
 
 async def poster(query) -> None:
     keyboard = [
         [
-            InlineKeyboardButton("На Главную", callback_data="main")
+            buttons["buy_ticket"]
         ],
         [
-            InlineKeyboardButton("Купить Билет", callback_data="buy_ticket")
+            buttons["about_event"]
+        ],
+        [
+            buttons["main"]
         ]
     ]
-
     reply_markup = InlineKeyboardMarkup(keyboard)
-
-    await query.edit_message_text("***Афиша КБрд***", reply_markup=reply_markup)
+    await query.edit_message_text(texts["poster"], reply_markup=reply_markup)
 
 
 async def buy_ticket(query) -> None:
     keyboard = [
         [
-            InlineKeyboardButton("На Главную", callback_data="main")
+            buttons["back_to_poster"]
         ],
     ]
-
     reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text(texts["buy_ticket"], reply_markup=reply_markup)
 
-    await query.edit_message_text("***Операция покупки билета***", reply_markup=reply_markup)
+
+async def event_info(query) -> None:
+    keyboard = [
+        [
+            buttons["back_to_poster"]
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text(texts["about_event"], reply_markup=reply_markup)
 
 
 async def other_funcs(query) -> None:
     keyboard = [
         [
-            InlineKeyboardButton("На Главную", callback_data="main")
-        ],
+            buttons["main"]
+        ]
     ]
-
     reply_markup = InlineKeyboardMarkup(keyboard)
-
-    await query.edit_message_text("ПИЗДА", reply_markup=reply_markup)
+    await query.edit_message_text(texts["other_funcs"], reply_markup=reply_markup)
 
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -111,11 +143,16 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await main_page(query)
     if query.data == "poster":
         await poster(query)
+    if query.data == "support":
+        await support(query)
     if query.data == "buy_ticket":
         await buy_ticket(query)
+    if query.data == "about_event":
+        await event_info(query)
+    if query.data == "back_to_poster":
+        await poster(query)
     if query.data == "other_funcs":
         await other_funcs(query)
-    # await query.edit_message_text(text=f"Selected option: {query.data}")
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
